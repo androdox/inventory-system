b00ae6d531dc74fbf1f4f1ddc837e67188078b63
androdox <niko.andrade.p@gmail.com>
2024-12-31 23:52:30 -0500

    feat: :sparkles: se agregan nuevas funciones al controller

--- modules/product/product.controller.js
+++ modules/product/product.controller.js
@@ -3,8 +3,7 @@ const productService = require('./product.services');
 async function getProducts(req, res) {
   try {
     const products = await productService.fetchAllProducts();
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(products));
+    res.status(200).json(products)
   } catch (error) {
     res.writeHead(500, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ error: 'Error retrieving products' }));
@@ -14,12 +13,31 @@ async function getProducts(req, res) {
 async function createProduct(req, res) {
   try {
     const newProduct = await productService.addProduct(req.body);
-    res.writeHead(201, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(newProduct));
+    res.status(201).json(newProduct)
   } catch (error) {
     res.writeHead(500, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ error: 'Error creating product' }));
   }
 }
 
-module.exports = { getProducts, createProduct };
+async function updateProduct(req, res) {
+  try {
+    const updateProduct = await productService.updateProduct(req.params.id, req.body);
+    res.status(200).json(updateProduct)
+  } catch (error) {
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Error updating product' }));
+  }
+}
+
+async function deleteProduct(req, res) {
+  try {
+    const deleteProduct = await productService.deleteProduct(req.params.id);
+    res.status(200).json(deleteProduct)
+  } catch (error) {
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Error deleting product' }));
+  }
+}
+
+module.exports = { getProducts, createProduct , updateProduct, deleteProduct };
