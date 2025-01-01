86a0a6830299e73f4765a1c74642ab64da091f6f
androdox <niko.andrade.p@gmail.com>
2024-12-31 23:51:54 -0500

    feat: :sparkles: se agregan nuevas funciones al service

--- modules/product/product.services.js
+++ modules/product/product.services.js
@@ -3,7 +3,7 @@ const productModel = require('./product.model');
 async function fetchAllProducts() {
   try {
     const result = await productModel.getAllProducts();
-    return result.rows;
+    return result;
   } catch (error) {
     console.error('Error fetching products:', error.message);
     throw new Error('Could not fetch products');
@@ -13,11 +13,30 @@ async function fetchAllProducts() {
 async function addProduct(data) {
   try {
     const result = await productModel.createProduct(data);
-    return result.rows[0];
+    return result;
   } catch (error) {
     console.error('Error adding product:', error.message);
     throw new Error('Could not add product');
   }
 }
 
-module.exports = { fetchAllProducts, addProduct };
+async function updateProduct(id, data) {
+  try {
+    const result = await productModel.updateProduct(id, data);
+    return result;
+  } catch (error) {
+    console.error('Error updating product:', error.message);
+    throw new Error('Could not update product');
+  }
+}
+
+async function deleteProduct(id) {
+  try {
+    const result = await productModel.deleteProduct(id);
+    return result;
+  } catch (error) {
+    console.error('Error deleting product:', error.message);
+    throw new Error('Could not delete product');
+  }
+}
+module.exports = { fetchAllProducts, addProduct, updateProduct, deleteProduct };
